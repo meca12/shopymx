e53d30f38c06be0e4c3c2b60a36810116c6f8b50
meca12 <itit.martha.avil@gmail.com>
2024-03-06 23:25:00 -0600

    estructura y navs

---
+++ src/app/patterns/patterns.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavigatorComponent } from './navigator/navigator.component';
+
+
+
+@NgModule({
+  declarations: [
+    NavigatorComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class PatternsModule { }
